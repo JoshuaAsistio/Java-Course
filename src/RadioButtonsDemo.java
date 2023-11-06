9719a802a78f44bb385b532893eb6e345b4acb32
Elijah Joshua Asistio <salazarelijahjoshuaasistio@gmail.com>
2023-11-05 16:14:22 -1200

    Added the RadioButtonsDemo file

--- src/RadioButtonsDemo.java
+++ src/RadioButtonsDemo.java
@@ -2,25 +2,45 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JRadioButton;
 
 public class RadioButtonsDemo {
 
 	public static void main(String[] args) {
-		new MyFrame();
+		// TODO Auto-generated method stub
+		new Frame();
 	}
 
 }
 
 class MyFrame extends JFrame implements ActionListener {
 	MyFrame() {
+class Frame extends JFrame implements ActionListener {
+	JRadioButton pizzaButton;
+	JRadioButton burgerButton;
+	JRadioButton friesButton;
+	
+	Frame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(420, 420);
 		setLayout(new FlowLayout());
 		
-		JRadioButton pizzaButton = new JRadioButton("Pizza");
-		JRadioButton burgerButton = new JRadioButton("Burger");
-		JRadioButton friesButton = new JRadioButton("Fries");
+		pizzaButton = new JRadioButton("Pizza");
+		burgerButton = new JRadioButton("Burger");
+		friesButton = new JRadioButton("Fries");
+		
+		pizzaButton.addActionListener(this);
+		burgerButton.addActionListener(this);
+		friesButton.addActionListener(this);
+		
+		ButtonGroup buttonGroup = new ButtonGroup();
+		
+		buttonGroup.add(pizzaButton);
+		buttonGroup.add(burgerButton);
+		buttonGroup.add(friesButton);
 		
 		add(pizzaButton);
 		add(burgerButton);
@@ -32,7 +52,12 @@ class MyFrame extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if (e.getSource() == pizzaButton) {
+			System.out.println("You ordered Pizza.");
+		} else if (e.getSource() == burgerButton) {
+			System.out.println("You ordered Burger");
+		} else if (e.getSource() == friesButton) {
+			System.out.println("You ordered Fries");
+		}
 	}
 }
